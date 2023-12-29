0df363fab2f83f38ded6cc0dc1bdbe9e1319dd94
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-12-30 02:15:11 +0900

    update

--- rware/unit.py
+++ rware/unit.py
@@ -32,6 +32,64 @@ action_list.append((1))
 
 # エージェントの位置を初期化
 
+################## A Start #######################
+
+sx = env.agents[0].x  # [m]
+sy = env.agents[0].y  # [m]
+gx = 10.0  # [m]
+gy = 10.0  # [m]
+grid_size = 1.0  # [m]
+robot_radius = 0.1  # [m]
+
+ox, oy = [], []
+for i in range(-1, 11):
+    ox.append(i)
+    oy.append(-1.0)
+for i in range(-1, 11):
+    ox.append(11.0)
+    oy.append(i)
+for i in range(-1, 11):
+    ox.append(i)
+    oy.append(11.0)
+for i in range(-1, 11):
+    ox.append(-1.0)
+    oy.append(i)
+
+ox.append(11)
+oy.append(11)
+
+for i in range(len(env.shelfs)):
+  print(i)
+  print(env.shelfs[i].x)
+  print(env.shelfs[i].y)
+  if env.shelfs[i] in env.request_queue:
+    print("req")
+    gx = env.shelfs[i].x
+    gy = env.shelfs[i].y
+  else:
+    ox.append(env.shelfs[i].x)
+    oy.append(env.shelfs[i].y)
+
+    print("not req")
+ 
+
+
+if show_animation:  # pragma: no cover
+    plt.plot(ox, oy, ".k")
+    plt.plot(sx, sy, "og")
+    plt.plot(gx, gy, "xb")
+    plt.grid(True)
+    plt.axis("equal")
+
+a_star = AStarPlanner(ox, oy, grid_size, robot_radius)
+rx, ry = a_star.planning(sx, sy, gx, gy)
+
+if show_animation:  # pragma: no cover
+    plt.plot(rx, ry, "-r")
+    plt.pause(0.001)
+    plt.show()
+#################################################
+
 import time
 #for i in range(len(action_list)):
 while True:
